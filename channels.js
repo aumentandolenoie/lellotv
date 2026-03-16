25fd80e735a1711b1eca332a8374e9cca33b8e88
aumentandolenoie <emanuele_dantonio@icloud.com>
2026-03-16 18:49:19 +0100

    Update channels.js

--- channels.js
+++ channels.js
@@ -11,7 +11,7 @@ const channels = [
         name: "Vavoo",
       },
       {
-        url: "https://dlhd.sx/stream/stream-577.php",
+        url: "https://dlstreams.top/watch.php?id=577",
         extractor: true,
         name: "DaddyLive",
       },
