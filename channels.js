63b9233f58873b785c0e81c3b2e4b7fb91ac1684
aumentandolenoie <emanuele_dantonio@icloud.com>
2026-03-19 13:10:05 +0100

    Update channels.js
    refixed daddy f1 link

--- channels.js
+++ channels.js
@@ -11,7 +11,7 @@ const channels = [
         name: "Vavoo",
       },
       {
-        url: "https://dlstreams.top/stream/stream-577.php",
+        url: "https://dlhd.sx/stream/stream-577.php",
         extractor: true,
         name: "DaddyLive",
       },
